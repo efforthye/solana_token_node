c2ad78de748cc7fe8df1632f1ac1a7d4ea90c706
efforthye <efforthye@gmail.com>
2025-01-28 23:20:35 +0900

    chore: remove dist from git and update .gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
-import { config } from "./config";
-(async () => {
-    const connection = new Connection(clusterApiUrl(config.network), "confirmed");
-    console.log(`Connected to Solana network: ${config.network}`);
-})();
--- dist/services/token/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./fungible";
-export * from "./nft";
